8362058f710875b5298929412aec88c510adb167
Denis Lukov <denismassters@gmail.com>
2016-10-29 13:38:32 +0300

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FF-SVG-path-pushState-workaround
+
+Brutal workaround for [FF issue #652991](https://bugzilla.mozilla.org/show_bug.cgi?id=652991)
+
+May be useful for SPA who rely on SVG patterns `url(#...)` and history.pushState.
+Having this stack you may noticed FF adds ` none;` to the end of `fill/stroke` paths after changing route, causing SVG paths to break.
---
+++ index.js
@@ -0,0 +1,31 @@
+/*! FF-SVG-path-pushState-workaround - v1.0.0 - 2016-10-29
+* http://NeXTs.github.com/ff-svg-path-pushstate-workaround/
+* Copyright (c) 2016 Denis Lukov; Licensed MIT */
+
+/**
+ * FF SVG patterns path issue workaround
+ * https://bugzilla.mozilla.org/show_bug.cgi?id=652991
+ *
+ * May be deleted once FF releases v51
+ */
+
+;(function(name, definition) {
+    if (typeof module != 'undefined') module.exports = definition()
+    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+    else this[name] = definition()
+}('ff-svg-path-pushstate-workaround', function() {
+
+    var isFirefox = /firefox/i.test(navigator.userAgent), ffVersion
+    if(isFirefox) ffVersion = parseInt(navigator.userAgent.split('/').pop())
+    
+    return function() {
+        if( ! isFirefox || ffVersion >= 51) return
+
+        var patterns = document.querySelectorAll('[fill^="url(#"], [stroke^="url(#"]')
+        for(var i = 0, ii = patterns.length, pattern; i < ii; i++) {
+            pattern = patterns[i]
+            pattern.style.fill = pattern.style.fill
+            pattern.style.stroke = pattern.style.stroke
+        }
+    }
+}));
